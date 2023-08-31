a09dfdb6e8fe6fd41c924fdb9254dc1bf9f11175
devnrj07 <niraj.mukta123@gmail.com>
2023-08-31 22:13:47 +0530

    integrated worker APIs for like/unlike

--- src/hooks/useLikeButton.ts
+++ src/hooks/useLikeButton.ts
@@ -1,18 +1,52 @@
-import React, { useState } from "react";
+import {
+  decrementLike,
+  getLikesCount,
+  incrementLike,
+} from "@/utils/APIRequests";
+import { useEffect, useState } from "react";
 
 const useLikeButton = () => {
   const [isLiked, setIsLiked] = useState(false);
   const [likesCount, setLikesCount] = useState(0);
 
   //fetch likes from DB on change in isLiked
+  useEffect(() => {
+    //localstorage has key -> setIsLiked(true)
+    (async () => {
+      const data = await getLikesCount();
+      if (data && data.status === 200) setLikesCount(data.likesCount);
+      //also update localstorage
+    })();
+  }, [isLiked]);
 
   //update DB
 
-  const handleLikeButtonClick = () => {
+  const handleLikeButtonClick = async () => {
     console.log("button clicked.");
     setIsLiked(!isLiked);
-    if (isLiked) setLikesCount((prev) => prev - 1);
-    else setLikesCount((prev) => prev + 1);
+    //if (isLiked) setLikesCount((prev) => prev - 1);
+    // else setLikesCount((prev) => prev + 1);
+    try {
+      if (isLiked) {
+        setLikesCount((prev) => prev - 1);
+        const data = await decrementLike();
+        if (data && data.status === 200) {
+          console.log("decrement success");
+          setLikesCount(data.likesCount);
+        }
+      } else {
+        setLikesCount((prev) => prev + 1);
+        const data = await incrementLike();
+        if (data && data.status === 200) {
+          console.log("increment success");
+          setLikesCount(data.likesCount);
+        }
+      }
+    } catch (err) {
+      //roll back
+      if (isLiked) setLikesCount((prev) => prev + 1);
+      else setLikesCount((prev) => prev - 1);
+    }
   };
 
   return {
---
+++ src/utils/constants/index.ts
@@ -0,0 +1,4 @@
+const BASEURL = "https://likes_count.hjjujwapmumwiuaiba.workers.dev";
+export const GET_LIKES_COUNT = `${BASEURL}/getLikes`;
+export const ADD_LIKE = `${BASEURL}/incrementLikes`;
+export const ADD_UNLIKE = `${BASEURL}/decrementLikes`;
